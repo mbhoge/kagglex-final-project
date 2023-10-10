22bced4577d1fa64946bad9f897658a44373d662
mbhoge <bhoge.m@outlook.com>
2023-10-10 17:35:52 +0530

    RAG mini

--- final_project/main.py
+++ final_project/main.py
@@ -15,7 +15,8 @@ class GenAILearningPathIndex:
     def __init__(self, data_path):
         load_dotenv()  # Load .env file
         # Rest of your code goes here
-        self.my_key = os.getenv("OPENAI_API_KEY")
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.pinecone_api_key = os.getenv("PINECONE_API_KEY")
         self.data_path = data_path
         self.data = None
         self.vectorizer = None
@@ -31,22 +32,19 @@ class GenAILearningPathIndex:
         self.text = text_splitter.split_documents(document)
     
     def getllm(self):
-        llm = OpenAI(temperature=0.1, openai_api_key=self.my_key)
-        result = llm("what is AI?")
-        print(result)
+        llm = OpenAI(temperature=0.1, openai_api_key=self.openai_api_key)
         
     def getembeddings(self):
-        self.embeddings = OpenAIEmbeddings(openai_api_key=self.my_key)
+        self.embeddings = OpenAIEmbeddings(openai_api_key=self.openai_api_key)
     
     def pinecone_init(self):
-        pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="asia-southeast1-gcp-free")
+        pinecone.init(api_key=self.pinecone_api_key, environment="asia-southeast1-gcp-free")
         
 
 if __name__=='__main__':
-    data_path="C:\\Users\\Manish_Bhoge\\OneDrive - EPAM\\Tut\\Kaggle-Mentership-Program\\Project - GenAI\\kagglex-final-project\\final_project\\Learning_Pathway_Index_1.csv"
+    current_directory = os.getcwd()
+    data_path = current_directory + "\\final_project\\Learning_Pathway_Index_1.csv" 
     GenAI_project = GenAILearningPathIndex(data_path)
-    # print(f"Key is : {GenAI_project.my_key}")
-    # GenAI_project.text = "what is AI?"
     GenAI_project.getllm()
     GenAI_project.load_data()
     GenAI_project.getembeddings()
@@ -58,9 +56,8 @@ if __name__=='__main__':
         llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever()
     )
     
-    query = "Give me Machine Learning Course with 10 min duration"
+    query = "Give me Machine Learning Course with 10 min duration. Can you suggest the similar courses on the coursera?"
     result = qa({"query":query})
     print(result)
-    #print(len(text))
     
     
