ad7dd12ae2dfa647bc530aff92eb6715486afb19
mbhoge <bhoge.m@outlook.com>
2023-10-15 18:14:44 +0530

    Modified Prompt Template

--- final_project/main.py
+++ final_project/main.py
@@ -12,21 +12,41 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.vectorstores import FAISS
 from interface import app
+from langchain.prompts import PromptTemplate
 import streamlit as st
        
 # Define GenAI class
 class GenAILearningPathIndex:
-    def __init__(self, data_path):
+    def __init__(self):
         load_dotenv()  # Load .env file
         self.openai_api_key = os.getenv("OPENAI_API_KEY")
         self.pinecone_api_key = os.getenv("PINECONE_API_KEY")
-        self.data_path = data_path
+        self.data_path = os.getcwd() + "\\Learning_Pathway_Index.csv"
         self.data = None
         self.vectorizer = None
         self.model = None
         self.text = None
         self.embeddings = None
         self.faiss_vectorstore = None
+        self.llm = OpenAI(temperature=0.1, openai_api_key=self.openai_api_key)
+        # Setting up the project
+       
+        # Initialize the Class
+        # Load the data
+        self.load_data()
+        # Get the embeddings
+        self.getembeddings()
+        # Initialize pinecone
+        # self.pinecone_init()
+        # Create the pincone index
+        # self.create_index()
+        # Create the QA model for pinecone
+        # qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+        # Test the pinecone model
+        # query = "Give me Machine Learning Course with 10 min duration. Are there any similar courses on coursera?"
+        # result = qa({"query": query})
+        # print(result)
+        self.faiss_index()
 
     def load_data(self):
         # Load your dataset (e.g., CSV, JSON, etc.)
@@ -34,9 +54,7 @@ class GenAILearningPathIndex:
         document = loader.load()
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
         self.text = text_splitter.split_documents(document)
-    
-    def getllm(self):
-        llm = OpenAI(temperature=0.1, openai_api_key=self.openai_api_key)
+           
         
     def getembeddings(self):
         self.embeddings = OpenAIEmbeddings(openai_api_key=self.openai_api_key)
@@ -49,53 +67,35 @@ class GenAILearningPathIndex:
         return docsearch 
     
     def faiss_index(self):
-        embeddings = OpenAIEmbeddings()
         vectorstore = FAISS.from_documents(self.text, self.embeddings)
         vectorstore.save_local("faiss_learning_path_index")
-        faiss_vectorstore = FAISS.load_local("faiss_learning_path_index", self.embeddings)
-        self.faiss_vectorstore = faiss_vectorstore     
+        faiss_vectorstore = FAISS.load_local("faiss_learning_path_index"
+                                             , self.embeddings)
+        self.faiss_vectorstore = faiss_vectorstore
 # Class definition ends here
 
 if __name__=='__main__':
     var = app()
     # st.write(f"The stored variable is: {var}")
     
-    # Setting up the project
-    current_directory = os.getcwd()
-    data_path = current_directory + "\\final_project\\Learning_Pathway_Index.csv"
+    # # Setting up the project
+    # current_directory = os.getcwd()
+    # data_path = current_directory + "\\Learning_Pathway_Index.csv"
     # Initialize the Class
-    GenAI_project = GenAILearningPathIndex(data_path)
-    # Load the data
-    GenAI_project.load_data()
-    # Get the embeddings
-    GenAI_project.getembeddings()
-    # Initialize pinecone
-    GenAI_project.pinecone_init()
-    # Create the pincone index
-    GenAI_project.create_index()
-    # Create the QA model for pinecone
-    # qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
-    # Test the pinecone model
-    # query = "Give me Machine Learning Course with 10 min duration. Are there any similar courses on coursera?"
-    # result = qa({"query": query})
-    # print(result)
-    GenAI_project.faiss_index()
-    # Create the QA model for pinecone
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=GenAI_project.faiss_vectorstore.as_retriever())
+    GenAI_project = GenAILearningPathIndex()
+    
+    prompt_template = """Use the following template to answer the question at the end. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    {context}
+    Question: {question}"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context","question"])
+    chain_type_kwargs = {"prompt": PROMPT}
+    qa = RetrievalQA.from_chain_type(llm=GenAI_project.llm, chain_type="stuff", 
+                                     retriever=GenAI_project.faiss_vectorstore.as_retriever(),
+                                     chain_type_kwargs=chain_type_kwargs)
     # Test the FAISS model
-    res = qa.run("{var}.")
+    # print(f'FAISS model test: {var} {type(var)}')
+    # res = qa.run(f'"{var}."')
+    
+    res = qa.run(var)
     st.write(res)
-    #print(res)
-    # comment out the below code to run the streamlit app       
-    
-    # llm = OpenAI(model="text-davinci-003", temperature=0.9)
-    # prompt = PromptTemplate(
-    #    input_variables=["user_input"],
-    #    template="{user_input}. Can you also give ?",
-    # )
-
-    # chain = LLMChain(llm=llm, prompt=prompt)
-
-    # Run the chain only specifying the input variable.
-    # print(chain.run("eco-friendly water bottles"))
-    
